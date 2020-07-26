68021142c368ad3bb65c4c095fe7f60eb8888df8
Fernando Michea <fernandomichea@MacBook-Air-de-Fernando.local>
2020-07-25 20:07:51 -0400

    sincronizando

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
 const express=require('express');
-
 const routes=require('./routes')
 const mongoose=require('mongoose')
 const bodyParser=require('body-parser')
+require('dotenv').config({path:'variables.env'})
 const cors =require('cors');
-
-
+const app =express()
 mongoose.Promise=global.Promise;
 mongoose.connect('mongodb://localhost/Dental',
 {
@@ -13,12 +12,40 @@ mongoose.connect('mongodb://localhost/Dental',
 
 })
 
-const app =express()
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
-app.use(cors());
 
+const whitelist =[process.env.FRONTEND_URL]
+const corsOption={
+    origin:(origin,callBack)=>
+    {
+
+
+        console.log("el origen "+origin);
+        
+        // revisar si la peticion proveniente del servidor esta en la whitelist
+
+        const existe =whitelist.some(dominio=>dominio===origin);
+        if(existe)
+        {
+
+            callBack(null,true)
+        }else{
+
+            callBack(new Error('no permitido por los suoer cors'));
+            
+        }
+    }
+
+}
+app.use(cors(corsOption));
 app.use('/',routes());
+const host =process.env.HOST || 'localhost'
+const port =process.env.PORT || 5000
+app.listen(port,host,()=>
+{
+    console.log(process.env.DB_URL);
+    console.log(process.env.FRONTEND_URL);
 
-app.listen(5001)
+})
